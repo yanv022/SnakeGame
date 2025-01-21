2e42be2f49a97433425f8c59462419b0d71de66e
franksosterne <167037778+franksosterne@users.noreply.github.com>
2025-01-21 08:50:19 +0100

    Game-Over-Bildschirm

--- scr/snakezero.jl
+++ scr/snakezero.jl
@@ -8,3 +8,15 @@ function draw_start_screen()
     subtitle.pos = (WIDTH / 2 - 130, HEIGHT / 2 + 20)
     draw(subtitle)
 end
+
+
+# Zeichnet den Game-Over-Bildschirm mit der Endpunktzahl.
+function draw_game_over_screen()
+    draw(Rect(0, 0, WIDTH, HEIGHT), colorant"black", fill = true)
+    game_over_title = TextActor("GAME OVER", "comicbd"; font_size = 48, color = Int[255, 0, 0, 255])
+    game_over_title.pos = (WIDTH / 2 - 150, HEIGHT / 2 - 50)
+    draw(game_over_title)
+    restart_message = TextActor("Klicke zum Neustart.", "comicbd"; font_size = 24, color = Int[200, 200, 200, 255])
+    restart_message.pos = (WIDTH / 2 - 130, HEIGHT / 2 + 20)
+    draw(restart_message)
+end
